fe957dad4f716e3b0a90477c9ee3aca7d75f300f
Jake Miller <hillsighed@gmail.com>
2017-05-25 09:49:41 -0600

    finishes navbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log('Hi from Main')
+console.log('Hi from Main');
+
+// Toggle Mobile Menu
+$('.nav-toggle').click((evt) => {
+  console.log('clicked')
+
+  if($('.nav-toggle').hasClass('is-active')) {
+    $('.nav-toggle').removeClass('is-active')
+  } else {
+    $('.nav-toggle').addClass('is-active')
+  }
+
+  if($('.nav-menu').hasClass('is-active')) {
+    $('.nav-menu').removeClass('is-active')
+  } else {
+    $('.nav-menu').addClass('is-active')
+  }
+
+})
